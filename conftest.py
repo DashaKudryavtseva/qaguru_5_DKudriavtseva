4ce3faf82a9267421460d264ec39cf80a85cee6b
Daria Kudriavtseva <situation96@mail.ru>
2023-04-10 16:50:19 +0300

    Cancelled work

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture
+def browser_configuration():
+    browser.config.hold_browser_open = True
+    browser.config.window_height = 1080
+    browser.config.window_width = 720
--- test_fill_form.py
+++ test_fill_form.py
@@ -1,18 +1,38 @@
-from selene import browser, have, be
+import os
+from selene import browser, be
 
 
-def test_fill():
-    browser.config.hold_browser_open = True
+
+def test_fill(browser_configuration):
+    # browser.config.browser_name = "firefox"
+
     browser.open("https://demoqa.com/automation-practice-form")
+
     browser.element('#firstName').type('Maria').press_enter()
     browser.element('#lastName').type('Ivanova').press_enter()
+
     browser.element('#userEmail').type('examplemail@mail.com').press_enter()
     # browser.element('[id="gender-radio-2"]').should(be.clickable).click()
     browser.element('[for="gender-radio-2"]').click()
     browser.element('#userNumber').type('8987654321')
+
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').type('January')
     browser.element('.react-datepicker__year-select').type('1996')
     browser.element(f'.react-datepicker__day--0{"01"}').click()
+
     browser.element('#subjectsInput').set_value("English").press_enter()
     browser.element('[for ="hobbies-checkbox-1"]').click()
+    # browser.element('#uploadPicture"]').send_keys(os.getcwd() + '/pict.jpg')
+    browser.element('#uploadPicture').send_keys(os.getcwd() + '\example.png')
+    # ---------
+    # browser.config.driver.capabilities.keys(keys.Control, "+")
+    browser.element('#currentAddress').type("Sport Street, 140").press_enter()
+    browser.element('#state').should(be.clickable).click()
+    browser.element('#react-select-3-input').set_value("NCR").press_enter()
+    browser.element('#city').should(be.clickable).click()
+    browser.element('#react-select-4-input').set_value("Delhi").press_enter()
+    browser.element('[id="submit"]').should(be.clickable).click()
+
+
+
